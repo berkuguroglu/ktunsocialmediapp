41704a2234f289cea20607d8f35d2bb30c8ec38d
berkuguroglu <expirty.bu@gmail.com>
2019-04-20 01:54:05 +0300

    ktunframework

---
+++ app/src/main/java/com/app/ktun/ktunhome/MainActivity.java
@@ -0,0 +1,133 @@
+package com.app.ktun.ktunhome;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.app.ktun.ktunhome.Fragments.FragmentStateAdapter;
+import com.app.ktun.ktunhome.Fragments.graduateFragment;
+import com.app.ktun.ktunhome.Fragments.homeFragment;
+import com.app.ktun.ktunhome.Fragments.loginFragment;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener, loginFragment.OnFragmentInteractionListener, homeFragment.OnFragmentInteractionListener, graduateFragment.OnFragmentInteractionListener {
+
+    ViewPager fragment_pager;
+    FragmentStateAdapter adapter;
+    ImageButton home_button, student_button, graduate_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        fragment_pager = findViewById(R.id.adapter_pager);
+        adapter = new FragmentStateAdapter(getSupportFragmentManager());
+        fragment_pager.setAdapter(adapter);
+        home_button = findViewById(R.id.home);
+        student_button = findViewById(R.id.student);
+        graduate_button = findViewById(R.id.graduate);
+        home_button.setOnClickListener(this);
+        student_button.setOnClickListener(this);
+        graduate_button.setOnClickListener(this);
+
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == this.findViewById(R.id.student))
+            this.fragment_pager.setCurrentItem(0);
+        else if(v == this.findViewById(R.id.home))
+            this.fragment_pager.setCurrentItem(1);
+        else if(v == this.findViewById(R.id.graduate))
+            this.fragment_pager.setCurrentItem(2);
+    }
+}
